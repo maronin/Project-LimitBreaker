15fed06583c715b553ee502be70d28efcdb946dd
Alex Craig <andeic@hotmail.com>
2013-01-25 22:04:34 -0500

    made it so that when an exercise is selected its related data is put into the form

--- SystemExercise/manageExerciseExperience.aspx.cs
+++ SystemExercise/manageExerciseExperience.aspx.cs
@@ -8,26 +8,67 @@ using System.Web.UI.WebControls;
 public partial class systemExercise_manageExerciseExperience : System.Web.UI.Page
 {
     ExperienceManager expMngr = new ExperienceManager();
+    SystemExerciseManager manager = new SystemExerciseManager();
 
     protected void Page_Load(object sender, EventArgs e)
     {
+        noExpLbl.Text = "";
+
         if (viewExerciseExp.ddlCount != 0)
         {
             manageExperienceMultiView.ActiveViewIndex = 1;
             ExerciseExp selectedExercise = expMngr.getExerciseExpByExerciseName(viewExerciseExp.ddlValue);
+            Exercise exercise = manager.getExercise(viewExerciseExp.ddlValue);
 
             try
             {
                 baseTxtBox.Text = selectedExercise.baseExperience.ToString();
-                timeTxtBox.Text = selectedExercise.timeModifier.ToString();
-                weightTxtBox.Text = selectedExercise.weightModifier.ToString();
-                repTxtBox.Text = selectedExercise.repModifier.ToString();
-                distanceTxtBox.Text = selectedExercise.distanceModifier.ToString();
+
+                if (exercise.time == true)
+                {
+                    timeTxtBox.Enabled = true;
+                    timeTxtBox.Text = selectedExercise.timeModifier.ToString();
+                }
+                else
+                {
+                    timeTxtBox.Enabled = false;
+                }
+
+                if (exercise.weight == true)
+                {
+                    weightTxtBox.Enabled = true;
+                    weightTxtBox.Text = selectedExercise.weightModifier.ToString();
+                }
+                else
+                {
+                    weightTxtBox.Enabled = false;
+                }
+
+                if (exercise.rep == true)
+                {
+                    repTxtBox.Enabled = true;
+                    repTxtBox.Text = selectedExercise.repModifier.ToString();
+                }
+                else
+                {
+                    repTxtBox.Enabled = false;
+                }
+
+                if (exercise.distance == true)
+                {
+                    distanceTxtBox.Enabled = true;
+                    distanceTxtBox.Text = selectedExercise.distanceModifier.ToString();
+                }
+                else
+                {
+                    distanceTxtBox.Enabled = false;
+                }
             }
 
             catch (Exception ex)
             {
                 noExpLbl.Text = "This exercise does not have any experience values associated with it yet";
+                noExpLbl.Text = ex.Message + Environment.NewLine + ex.StackTrace;
             }
         }
 
