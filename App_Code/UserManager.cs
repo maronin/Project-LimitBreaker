69c459c2d1a6cf9561401d1464cd4a2385d0340b
Lynart <vince.lee.lien@gmail.com>
2013-01-28 17:40:39 -0500

    Initial workings on LimitBreaker
    Need to finish createUser.aspx.cs

---
+++ App_Code/UserManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for UserManager
+/// </summary>
+public class UserManager
+{
+    System.Web.HttpApplication _context;
+    public UserManager()
+    {
+        //Reference to curent application instance
+        _context = System.Web.HttpContext.Current.ApplicationInstance;
+    }
+
+    public bool createNewLimitBreaker(String username, String email, String gender, Int16 age, Double weight, Double height)
+    {
+        bool rc = false;
+
+        using (var context = new Layer2Container())
+        {
+            LimitBreaker newLimitBreaker = new LimitBreaker();
+            try
+            {
+                if ((context.LimitBreakers.FirstOrDefault(limitbreaker => limitbreaker.username == username).username == username))
+                    rc = false;
+            }
+            catch (NullReferenceException e)
+            {
+                newLimitBreaker.username = username;
+                newLimitBreaker.gender = gender;
+                newLimitBreaker.age = age;
+                newLimitBreaker.email = email;
+
+                if (createNewStats(weight, height))
+                {
+                    context.LimitBreakers.AddObject(newLimitBreaker);
+                    context.SaveChanges();
+                    rc = true;
+                }
+            }
+            return rc;
+        }
+    }
+
+    public bool createNewStats(Double weight, Double height)
+    {
+        bool rc = false;
+        using (var context = new Layer2Container())
+        {
+            Statistics stats = new Statistics();
+
+            stats.level = 1;
+            stats.experience = 1;
+            stats.weight = weight;
+            stats.height = height;
+            stats.rmr = 0;
+            stats.bmi = 0;
+            stats.vo2MAX = 0;
+        }
+        return rc;
+    }
+}
--- User/createUser.aspx.cs
+++ User/createUser.aspx.cs
@@ -7,13 +7,23 @@ using System.Web.UI.WebControls;
 
 public partial class User_createUser : System.Web.UI.Page
 {
+    UserManager manager = new UserManager();
     protected void Page_Load(object sender, EventArgs e)
     {
 
     }
     protected void Create_Click(object sender, EventArgs e)
     {
+        String userName=((TextBox)LoginView1.FindControl("userName")).Text;
+        String password=((TextBox)LoginView1.FindControl("password")).Text;
+        String email=((TextBox)LoginView1.FindControl("email")).Text;
+        String gender=((RadioButtonList)LoginView1.FindControl("gender")).SelectedValue;
+        Double 
+
         System.Web.Security.MembershipCreateStatus status;
-        System.Web.Security.Membership.CreateUser(((TextBox)LoginView1.FindControl("userName")).Text, ((TextBox)LoginView1.FindControl("password")).Text, ((TextBox)LoginView1.FindControl("email")).Text, "WTF", "123 answer", true, out status);
+        System.Web.Security.Membership.CreateUser(userName, password, email, "none", "none", true, out status);
+        System.Web.Security.Roles.AddUserToRole(userName, "user");
+
+        manager.createNewLimitBreaker(userName, email, 
     }
 }
