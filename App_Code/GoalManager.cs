baf80ada57831175f3aaddc5dfb45b8f626391a4
Alex Craig <andeic@hotmail.com>
2013-02-03 16:44:55 -0500

    added exerciseGoalManager class

---
+++ App_Code/GoalManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for GoalManager
+/// </summary>
+public class GoalManager
+{
+	public GoalManager()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public List<ExerciseGoal> getAllExerciseGoalsFromUser(string userName)
+    {
+        using (var context = new Layer2Container())
+        {
+            return context.ExerciseGoals.Where(s => s.LimitBreaker.username == userName).ToList();
+        }
+    }
+}
