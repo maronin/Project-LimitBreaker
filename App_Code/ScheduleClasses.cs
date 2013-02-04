710747706b49b47b1756286c6ec837a6ffe5e1ac
Mark Aronin <steroidcat@gmail.com>
2013-02-04 17:16:02 -0500

    changed the custom class naming

---
+++ App_Code/ScheduleClasses.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for customBusinessObjectClasses
+/// </summary>
+public class scheduledItem
+{
+    public scheduledItem()
+    {
+
+    }
+
+    public virtual string itemName
+    {
+        get;
+        set;
+    }
+
+    public virtual DateTime startTime
+    {
+        get;
+        set;
+    }
+    public virtual LimitBreaker user
+    {
+        get;
+        set;
+    }
+    public virtual bool isExericse
+    {
+        get;
+        set;
+    }
+
+
+
+}
