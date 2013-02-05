9af1e7358692f580fac97e1c80dd4e174c156e38
Mark Aronin <steroidcat@gmail.com>
2013-02-05 00:40:10 -0500

    did this ui in about an hour. Looks waaaay better than our old shit. I'll need to clean up a lot of useless css. There a lot of old stuff we are not using any more from our old css file

--- ui/mp/MasterPage.master.cs
+++ ui/mp/MasterPage.master.cs
@@ -7,32 +7,66 @@ using System.Web.UI.WebControls;
 
 public partial class ui_mp_MasterPage : System.Web.UI.MasterPage
 {
+   static bool[] selectedOption = new bool[6];
     protected void Page_Load(object sender, EventArgs e)
     {
         Page.MaintainScrollPositionOnPostBack = true;
+
+        if (selectedOption[0])
+            lnkSysExercises.CssClass = "selected";
+        else if (selectedOption[1])
+            lnkRoutines.CssClass = "selected";
+        else if (selectedOption[2])
+            lnkUser.CssClass = "selected";
+        else if (selectedOption[3])
+            lnkExperience.CssClass = "selected";
+        else if (selectedOption[4])
+            lnkSchedule.CssClass = "selected";
+        else if (selectedOption[5])
+            lnkLogin.CssClass = "selected";
+
+
     }
     protected void btnManageSysExercise_Click(object sender, EventArgs e)
     {
+
+        selectedOption[0] = true;
+        selectedOption[1] = selectedOption[2] = selectedOption[3] = selectedOption[4] = selectedOption[5] = false;
         Response.Redirect("~/SystemExercise/default.aspx");
+
     }
     protected void btnManageUserRoutines_Click(object sender, EventArgs e)
     {
+        selectedOption[1] = true;
+        selectedOption[0] = selectedOption[2] = selectedOption[3] = selectedOption[4] = selectedOption[5] = false;
         Response.Redirect("~/UserRoutines/default.aspx");
     }
-    protected void Button6_Click(object sender, EventArgs e)
+    protected void btnCreateUser(object sender, EventArgs e)
     {
+        selectedOption[2] = true;
+        selectedOption[1] = selectedOption[0] = selectedOption[3] = selectedOption[4] = selectedOption[5] = false;
         Response.Redirect("~/User/createUser.aspx");
+        
     }
-    protected void Button8_Click(object sender, EventArgs e)
+    protected void btnManageExperience(object sender, EventArgs e)
     {
+        selectedOption[3] = true;
+        selectedOption[1] = selectedOption[2] = selectedOption[0] = selectedOption[4] = selectedOption[5] = false;
         Response.Redirect("~/SystemExercise/manageExerciseExperience.aspx");
+        
     }
     protected void btnWorkOutSchedule_Click(object sender, EventArgs e)
     {
+        selectedOption[4] = true;
+        selectedOption[1] = selectedOption[2] = selectedOption[3] = selectedOption[0] = selectedOption[5] = false;
         Response.Redirect("~/WorkoutSchedule/default.aspx");
+        
     }
-    protected void Button9_Click(object sender, EventArgs e)
+    protected void btnLogin(object sender, EventArgs e)
     {
+        selectedOption[5] = true;
+        selectedOption[1] = selectedOption[2] = selectedOption[3] = selectedOption[4] = selectedOption[0] = false;
         Response.Redirect("~/login.aspx");
+       
     }
 }
