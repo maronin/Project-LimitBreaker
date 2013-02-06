bea4f5dee6df6f1847158b7446be8f4cae94ab58
Lynart <vince.lee.lien@gmail.com>
2013-02-06 18:20:40 -0500

    Initial user workings Note gigantic bugs: if not logged in, page will break if you create a new user, page will break
    Second bug can be fixed via auto login upon user creation
    First bug can be fixed via permissions

--- User/profile.aspx.cs
+++ User/profile.aspx.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
 
 public partial class User_profile : System.Web.UI.Page
 {
+    UserManager manager = new UserManager();
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        String username = Membership.GetUser().UserName;
+        Statistics userStats = manager.getStats(username);
+        alias.Text = username;
+        level.Text = Convert.ToString(userStats.level);
     }
 }
