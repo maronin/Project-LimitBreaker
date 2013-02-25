e9eafc9356526029a2bdbf2621748734d31ae1c5
Alex Craig <andeic@hotmail.com>
2013-02-25 17:23:50 -0500

    fixed levelup function

--- App_Code/ExperienceManager.cs
+++ App_Code/ExperienceManager.cs
@@ -201,16 +201,17 @@ public class ExperienceManager
             leveled = true;
         }
 
-        //Not sure if code below will work
+        //Not 100% sure if code below will work
         using (var context = new Layer2Container()) 
         {
-            LimitBreaker saveUser = user;
+            LimitBreaker saveUser = context.LimitBreakers.Where(s => s.username == user.username).FirstOrDefault();
+            saveUser.Statistics.experience = user.Statistics.experience;
+            saveUser.Statistics.level = user.Statistics.level;
             context.SaveChanges();
         }
 
         return leveled;
     }
-
 
     // Probably gonna have to make a function that uses the returned value from either 
     // calculateLoggedRoutineExperience() or calculateLoggedExerciseExperience() and gives it to the user that logged it
